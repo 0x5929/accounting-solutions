e6f9399e323e5a01df56053b494b89becb442095
kevin <work.kevin.ren@gmail.com>
2023-02-27 02:22:35 -0800

    removed the /registration via REST points, this feature is disabled, and can be done in admin panel by biz owner

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,10 +1,5 @@
 namespace :api do
     namespace :v1 do
-
-        scope :users, module: :users do
-            post '/', to: 'registration#create', as: :user_registration
-        end
-
         resources :documents
     end
 end
