efe94ef040e92702d3bb6813ecd6a6c83b487a14
kevin <work.kevin.ren@gmail.com>
2023-02-28 00:29:51 -0800

    Finished integrating templates to all pages Also, removed calling bootstrap css/js twice from rails also fixed the turbo link issues
    TODO:
    modify template contents, connect links

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
   def index
     @application = Doorkeeper::Application.find_by(name: 'Web Client')
 
+    # for dev purpose only, so we can get the client_id and client_secret to be exported to an .env file for frontend
     @application = {
       name: @application.name,
       client_id: @application.uid,
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
+// import * as bootstrap from "bootstrap"
+
+// fixes turbo link issues with legacy jquery code
+Turbo.session.drive = false
