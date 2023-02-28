1228df4a86319d9f78bcb7aa03ff897c2964efaf
kevin <work.kevin.ren@gmail.com>
2023-02-27 23:21:37 -0800

    successfully integrated javascript files, css, sass images, and font files to project.
    Index is done, finish the rest of pages!

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,9 @@
+import * as AOS from './aos'
+import * as counter from './counter'
+import * as custom from './custom'
+import * as glightbox from './glightbox.min.js'
+import * as  navbar from './navbar'
+import * as tinySlider from './tiny-slider'
+
+
+export { AOS, counter, custom, glightbox, navbar, tinySlider }
