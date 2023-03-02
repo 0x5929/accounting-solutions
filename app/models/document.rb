9dbe355cf71d964e678a06da8435be2d9c077347
kevin <work.kevin.ren@gmail.com>
2023-03-02 14:50:38 -0800

    addeding onto prev commit message, add necessary seeds to mess around with

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,5 @@ class Document < ApplicationRecord
   belongs_to :user
   validates_associated :user
   validates :user_id, :file_name, :file_location, presence: true
-  # validates :file_name, uniqueness: true
-  # validates :file_location, uniqueness: true
+
 end
